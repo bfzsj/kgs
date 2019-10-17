de96b0dbd7a5ac0479355b30f02bc341afb08b58
beifengzsj@163.com <beifengzsj@163.com>
2019-10-17 15:43:59 +0800

    提交

---
+++ build/precache-manifest.b9145e951a047ac98a46bca8ea934aed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a229d61e06692916b3773f3b8dab11e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "149f9792d4d34ff47bd3",
+    "url": "/static/css/main.5e4e9510.chunk.css"
+  },
+  {
+    "revision": "994d906ea3373cf0592b",
+    "url": "/static/js/2.a1a3d5e1.chunk.js"
+  },
+  {
+    "revision": "149f9792d4d34ff47bd3",
+    "url": "/static/js/main.f8964321.chunk.js"
+  },
+  {
+    "revision": "b1a6088872f136113b2a",
+    "url": "/static/js/runtime-main.e582b710.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
